52cb2002bf0c8722c4c6762a83fba7c21a8d1fce
Hussnain Cheema <cheemahussnain155@gmail.com>
2026-04-05 09:00:13 +0500

    Added SplashScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { colors } from '@/constants/theme'
+import Animated from 'react-native-reanimated'
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.neutral900} />
+      <Animated.Image
+        source={require('@/assets/images/splash.png')}
+      />
     </View>
   )
 }
 
 export default SplashScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: colors.neutral900,
+  }
+})
